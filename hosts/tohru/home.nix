7a0921806eae41edd37bc422bb6a21c540c5a8d1
Katherina Walshe-Grey <git@katherina.rocks>
2024-06-04 18:20:51 +0100

    [tohru] Add background image

--- hosts/tohru/home.nix
+++ hosts/tohru/home.nix
@@ -17,60 +17,83 @@
     ];
   };
 
-  home-manager.users.bluebird = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      fortune
-      htop
-      tree
+  home-manager.users.bluebird = { pkgs, ... }:
+    let
+      homeDirectory = "/home/bluebird";
+    in
+    {
+      home.homeDirectory = homeDirectory;
 
-      nil
-      nixpkgs-fmt
-    ];
+      home.packages = with pkgs; [
+        fortune
+        htop
+        tree
 
-    dconf = {
-      enable = true;
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    };
+        nil
+        nixpkgs-fmt
+      ];
 
-    programs.chromium.enable = true;
-
-    programs.git = {
-      enable = true;
-      userName = "Katherina Walshe-Grey";
-      userEmail = "git@katherina.rocks";
-    };
-
-    programs.vscode =
-      let
-        system = builtins.currentSystem;
-        sources = import ../../npins;
-        extensions = (import sources.nix-vscode-extensions).extensions.${system};
-      in
-      {
+      dconf = {
         enable = true;
-        enableExtensionUpdateCheck = false;
-        enableUpdateCheck = false;
-        package = pkgs.vscodium;
-        extensions = (with pkgs.vscode-extensions; [
-          jnoortheen.nix-ide
-          ms-python.python
-        ]) ++ (with extensions.open-vsx; [
-          robbowen.synthwave-vscode
-        ]);
-        mutableExtensionsDir = false;
-        userSettings = {
-          "extensions.autoUpdate" = false;
-          "git.autofetch" = true;
-          "nix.enableLanguageServer" = true;
-          "nix.serverPath" = "nil";
-          "nix.serverSettings".nil = {
-            diagnostics.ignored = [ "unused_binding" "unused_with" ];
-            formatting.command = [ "nixpkgs-fmt" ];
+        settings =
+          let
+            backgroundOptions = {
+              color-shading-type = "solid";
+              picture-options = "zoom";
+              picture-uri = "${homeDirectory}/.background-image";
+              primary-color = "#3a4ba0";
+              secondary-color = "#2f302f";
+            };
+          in
+          {
+            "org/gnome/desktop/background" = backgroundOptions // {
+              picture-uri-dark = backgroundOptions.picture-uri;
+            };
+            "org/gnome/desktop/screensaver" = backgroundOptions;
+            "org/gnome/desktop/interface".color-scheme = "prefer-dark";
           };
-          "workbench.colorTheme" = "SynthWave '84";
-        };
+      };
+      home.file.".background-image".source = ./background-image.jpg;
+
+      programs.chromium.enable = true;
+
+      programs.git = {
+        enable = true;
+        userName = "Katherina Walshe-Grey";
+        userEmail = "git@katherina.rocks";
       };
 
-    home.stateVersion = "23.11";
-  };
+      programs.vscode =
+        let
+          system = builtins.currentSystem;
+          sources = import ../../npins;
+          extensions = (import sources.nix-vscode-extensions).extensions.${system};
+        in
+        {
+          enable = true;
+          enableExtensionUpdateCheck = false;
+          enableUpdateCheck = false;
+          package = pkgs.vscodium;
+          extensions = (with pkgs.vscode-extensions; [
+            jnoortheen.nix-ide
+            ms-python.python
+          ]) ++ (with extensions.open-vsx; [
+            robbowen.synthwave-vscode
+          ]);
+          mutableExtensionsDir = false;
+          userSettings = {
+            "extensions.autoUpdate" = false;
+            "git.autofetch" = true;
+            "nix.enableLanguageServer" = true;
+            "nix.serverPath" = "nil";
+            "nix.serverSettings".nil = {
+              diagnostics.ignored = [ "unused_binding" "unused_with" ];
+              formatting.command = [ "nixpkgs-fmt" ];
+            };
+            "workbench.colorTheme" = "SynthWave '84";
+          };
+        };
+
+      home.stateVersion = "23.11";
+    };
 }
