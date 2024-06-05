570e376e2e895f51957505b507b5b59952859f9f
Katherina Walshe-Grey <git@katherina.rocks>
2024-06-05 13:35:33 +0100

    Remove pre-24.05 workaround for pinning <nixpkgs>

--- hive.nix
+++ hive.nix
@@ -1,15 +1,18 @@
 let sources = import ./npins;
 in {
-  meta = {
-    nixpkgs = sources.nixpkgs;
-  };
+  meta.nixpkgs = sources.nixpkgs;
 
   defaults = { pkgs, ... }: {
     imports = [
-      ./pinning.nix
       (import "${sources.home-manager}/nixos")
     ];
     deployment.replaceUnknownProfiles = false;
+    
+    # Make <nixpkgs> point systemwide to the pinned nixpkgs above
+    # https://jade.fyi/blog/pinning-nixos-with-npins/
+    nix.settings.experimental-features = "nix-command flakes";
+    nixpkgs.flake.source = sources.nixpkgs;
+    nix.nixPath = ["nixpkgs=flake:nixpkgs"];
   };
 
   tohru = { name, nodes, ... }: {
