0f824bc28471d827481357766342317af8afe754
Katherina Walshe-Grey <git@katherina.rocks>
2024-06-05 21:07:45 +0100

    [tohru] Modularise vscode config

---
+++ home/vscode.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.vscode =
+    let
+      system = builtins.currentSystem;
+      sources = import ../npins;
+      extensions = (import sources.nix-vscode-extensions).extensions.${system};
+    in
+    {
+      enable = true;
+      enableExtensionUpdateCheck = false;
+      enableUpdateCheck = false;
+      package = pkgs.vscodium;
+      extensions = (with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        ms-python.python
+      ]) ++ (with extensions.open-vsx; [
+        robbowen.synthwave-vscode
+      ]);
+      mutableExtensionsDir = false;
+      userSettings = {
+        "extensions.autoUpdate" = false;
+        "git.autofetch" = true;
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings".nil = {
+          diagnostics.ignored = [ "unused_binding" "unused_with" ];
+          formatting.command = [ "nixpkgs-fmt" ];
+        };
+        "workbench.colorTheme" = "SynthWave '84";
+      };
+    };
+
+  # Language servers etc
+  home.packages = with pkgs; [
+    nil
+    nixpkgs-fmt
+  ];
+}
--- hosts/tohru/home.nix
+++ hosts/tohru/home.nix
@@ -2,6 +2,10 @@
 
 {
   home-manager.users.qenya = { pkgs, ... }: {
+    imports = [
+      ../../home/vscode.nix
+    ];
+
     home.homeDirectory = config.users.users.qenya.home;
 
     home.packages = with pkgs; [
@@ -11,9 +15,6 @@
 
       bitwarden
       tor-browser-bundle-bin
-
-      nil
-      nixpkgs-fmt
     ];
 
     dconf = {
@@ -46,37 +47,6 @@
       userName = "Katherina Walshe-Grey";
       userEmail = "git@katherina.rocks";
     };
-
-    programs.vscode =
-      let
-        system = builtins.currentSystem;
-        sources = import ../../npins;
-        extensions = (import sources.nix-vscode-extensions).extensions.${system};
-      in
-      {
-        enable = true;
-        enableExtensionUpdateCheck = false;
-        enableUpdateCheck = false;
-        package = pkgs.vscodium;
-        extensions = (with pkgs.vscode-extensions; [
-          jnoortheen.nix-ide
-          ms-python.python
-        ]) ++ (with extensions.open-vsx; [
-          robbowen.synthwave-vscode
-        ]);
-        mutableExtensionsDir = false;
-        userSettings = {
-          "extensions.autoUpdate" = false;
-          "git.autofetch" = true;
-          "nix.enableLanguageServer" = true;
-          "nix.serverPath" = "nil";
-          "nix.serverSettings".nil = {
-            diagnostics.ignored = [ "unused_binding" "unused_with" ];
-            formatting.command = [ "nixpkgs-fmt" ];
-          };
-          "workbench.colorTheme" = "SynthWave '84";
-        };
-      };
 
     home.stateVersion = "23.11";
   };
