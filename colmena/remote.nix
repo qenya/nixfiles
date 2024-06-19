f464d022e523fb38e946a67b57042a4e5c975c03
Katherina Walshe-Grey <git@katherina.rocks>
2024-06-19 19:48:23 +0100

    Enable passwordless sudo on remote machines
    Closes #2

--- colmena/remote.nix
+++ colmena/remote.nix
@@ -3,8 +3,12 @@
 {
   deployment = {
     targetHost = "${name}.birdsong.network";
+    targetUser = "qenya";
     tags = [ "remote" ];
   };
+
+  # Required for remote builds
+  security.sudo.wheelNeedsPassword = false;
 
   imports = [
     ../common/openssh.nix
--- common/openssh.nix
+++ common/openssh.nix
@@ -12,9 +12,4 @@
   services.fail2ban.enable = true;
 
   networking.firewall.allowedTCPPorts = [ 22 ];
-
-  # Allow remote root login only from home network
-  # TODO: Find a less hacky way of doing remote deployment
-  users.users.root.openssh.authorizedKeys.keys = config.users.users.qenya.openssh.authorizedKeys.keys;
-  services.openssh.extraConfig = "Match Address 45.14.17.200\n    PermitRootLogin prohibit-password";
 }
