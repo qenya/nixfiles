a36c1d5e9f11a991b186fb122001c8f7bdacc400
Katherina Walshe-Grey <git@katherina.rocks>
2024-06-19 20:30:53 +0100

    Add key for tohru to agenix secrets file

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,6 @@
 let
-  yevaud = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHUAgyQhl390yUObLUI+jEbuNrZ2U6+8px628DolD+T";
-  systems = [ yevaud ];
+  tohru = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8wuGzF0Y7SaH9aimo3SmCz99MTQwL+rEVhx0jsueU root@tohru";
+  yevaud = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHUAgyQhl390yUObLUI+jEbuNrZ2U6+8px628DolD+T root@yevaud";
+  systems = [ tohru yevaud ];
 in
 { }
