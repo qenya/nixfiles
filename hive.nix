6fc5a2b1ea54207a13c292a52d664d6227dfe2fb
Katherina Walshe-Grey <git@katherina.rocks>
2024-06-19 19:39:00 +0100

    Fix CVE-2021-3156

--- hive.nix
+++ hive.nix
@@ -23,6 +23,7 @@ in {
       (import "${sources.home-manager}/nixos")
       (import "${sources.agenix}/modules/age.nix")
       ./pinning.nix
+      ./common/sudo.nix
       ./common/utilities.nix
       ./users/qenya.nix
     ];
