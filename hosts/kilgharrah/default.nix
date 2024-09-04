92f5b74584cf25ce13f9e13a9d991fb7ee48dd88
Katherina Walshe-Grey <git@qenya.tel>
2024-09-04 23:43:56 +0100

    kilgharrah: update deprecated option name

--- hosts/kilgharrah/default.nix
+++ hosts/kilgharrah/default.nix
@@ -28,7 +28,7 @@
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
   services.xserver.xkb.layout = "gb";
 
   services.printing.enable = true;
