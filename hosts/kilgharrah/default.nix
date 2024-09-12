a2cb35148c40c39fce067dbb16c8694ee6afc557
Katherina Walshe-Grey <git@qenya.tel>
2024-09-12 11:38:17 +0100

    pipewire-low-latency: split to reusable module

--- hosts/kilgharrah/default.nix
+++ hosts/kilgharrah/default.nix
@@ -24,6 +24,8 @@
   console.keyMap = "uk";
   services.xserver.xkb.layout = "gb";
 
+  qenya.services.pipewire.lowLatency.enable = true;
+
   age.secrets.user-password-kilgharrah-qenya.file = ../../secrets/user-password-kilgharrah-qenya.age;
   users.users.qenya.hashedPasswordFile = config.age.secrets.user-password-kilgharrah-qenya.path;
   users.users.qenya.extraGroups = [ "wheel" ];
