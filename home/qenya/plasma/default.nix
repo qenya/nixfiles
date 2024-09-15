7189fae10971563bda9dbbb21bfd0a9d992a52b1
Katherina Walshe-Grey <git@qenya.tel>
2024-09-15 17:40:19 +0100

    qenya: plasma: workaround for issue in randomcat's config

--- home/qenya/plasma/default.nix
+++ home/qenya/plasma/default.nix
@@ -1,11 +1,19 @@
 { config, lib, pkgs, osConfig, ... }:
 
 let
+  inherit (lib) mkIf;
   isPlasma = osConfig.services.desktopManager.plasma6.enable || osConfig.services.xserver.desktopManager.plasma5.enable;
 in
 {
-  programs.plasma.enable = isPlasma;
-  programs.plasma.overrideConfig = true;
+  # FIXME: this mkIf is necessary because home/qenya is imported into shaw here:
+  #  https://github.com/randomnetcat/nix-configs/blob/75d491dc6904475e43a820287edf3cf2f89abcfb/hosts/shaw/birdsong.nix#L74
+  #  shaw doesn't understand programs.plasma because randomcat doesn't import
+  #  plasma-manager, and is unwilling to because none of her machines run KDE.
+  #  This probably can't be fixed until we merge our configs completely.
+  programs = mkIf isPlasma {
+    plasma.enable = isPlasma;
+    plasma.overrideConfig = true;
+  };
 
   imports = [ ];
 }
