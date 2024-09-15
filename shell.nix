f4228d99261f19536f604d01aa2689f3f14e0ec6
Katherina Walshe-Grey <git@qenya.tel>
2024-09-15 16:37:30 +0100

    correct flake-compat hash

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ let
   shell = (import
     (fetchTarball {
       url = "https://github.com/edolstra/flake-compat/archive/refs/tags/v1.0.1.tar.gz";
-      sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5";
+      sha256 = "0m9grvfsbwmvgwaxvdzv6cmyvjnlww004gfxjvcl806ndqaxzy4j";
     })
     { src = ./.; }).shellNix;
 in
