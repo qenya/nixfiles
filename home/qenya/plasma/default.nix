d881607cb0b430e614ce2848c40f1e23cb0295a3
Katherina Walshe-Grey <git@qenya.tel>
2024-09-17 18:41:13 +0100

    Revert "qenya: plasma: workaround for issue in randomcat's config"
    This reverts commit 7189fae10971563bda9dbbb21bfd0a9d992a52b1.
    
    This didn't actually fix the issue, as it turns out mkIf still
    resolves the children of its attrset recursively. The longer-term
    solution is to export my home-manager config as a flake output,
    which I'm moving towards.

--- home/qenya/plasma/default.nix
+++ home/qenya/plasma/default.nix
@@ -1,19 +1,11 @@
 { config, lib, pkgs, osConfig, ... }:
 
 let
-  inherit (lib) mkIf;
   isPlasma = osConfig.services.desktopManager.plasma6.enable || osConfig.services.xserver.desktopManager.plasma5.enable;
 in
 {
-  # FIXME: this mkIf is necessary because home/qenya is imported into shaw here:
-  #  https://github.com/randomnetcat/nix-configs/blob/75d491dc6904475e43a820287edf3cf2f89abcfb/hosts/shaw/birdsong.nix#L74
-  #  shaw doesn't understand programs.plasma because randomcat doesn't import
-  #  plasma-manager, and is unwilling to because none of her machines run KDE.
-  #  This probably can't be fixed until we merge our configs completely.
-  programs = mkIf isPlasma {
-    plasma.enable = isPlasma;
-    plasma.overrideConfig = true;
-  };
+  programs.plasma.enable = isPlasma;
+  programs.plasma.overrideConfig = true;
 
   imports = [ ];
 }
