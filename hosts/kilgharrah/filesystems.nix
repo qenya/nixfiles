5ef9816a340520fceeeb1ad49a1e0530fa0975d2
Katherina Walshe-Grey <git@qenya.tel>
2024-09-23 23:22:48 +0100

    kilgharrah: enable zfs, luksdev

--- hosts/kilgharrah/filesystems.nix
+++ hosts/kilgharrah/filesystems.nix
@@ -2,8 +2,14 @@
 
 {
   boot.initrd.luks.devices = {
-    "enc".device = "/dev/disk/by-uuid/b414aaba-0a36-4135-a7e1-dc9489286acd";
+    "cryptroot".device = "/dev/disk/by-uuid/b414aaba-0a36-4135-a7e1-dc9489286acd";
   };
+
+  boot.supportedFilesystems = [ "zfs" ];
+
+  environment.etc.crypttab.text = ''
+    cryptstorage UUID=acda0e7a-069f-47c7-8e37-ec00e7cdde0f /root/luks-albion.key
+  '';
 
   fileSystems = {
     "/" = {
