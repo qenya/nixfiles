59bbcc165e7da1f615018eaea5ad1d1ea01671cd
Katherina Walshe-Grey <git@qenya.tel>
2024-09-26 12:15:49 +0100

    nginx: expand default CSP
    this is required to run wasm and web workers

--- common/nginx.nix
+++ common/nginx.nix
@@ -9,11 +9,11 @@
 
     appendHttpConfig = ''
       add_header Strict-Transport-Security "max-age=31536000; includeSubdomains; preload" always;
-      add_header Content-Security-Policy "default-src https: data: 'unsafe-inline'; object-src 'none'; base-uri 'none';" always;
+      add_header Content-Security-Policy "default-src https: data: blob: 'unsafe-inline' 'wasm-unsafe-eval'; object-src 'none'; base-uri 'none';" always;
       add_header Referrer-Policy strict-origin-when-cross-origin;
       add_header X-Frame-Options SAMEORIGIN;
       add_header X-Content-Type-Options nosniff;
-      add_header X-Clacks-Overhead "GNU Terry Pratchett";
+      add_header X-Clacks-Overhead "GNU Terry Pratchett" always;
       proxy_cookie_path / "/; secure; HttpOnly; SameSite=strict";
     '';
   };
