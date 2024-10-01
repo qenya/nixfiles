5e1cfc3997e074d1dc18bfadd2ef78e78de313d9
Katherina Walshe-Grey <git@qenya.tel>
2024-10-01 19:30:21 +0100

    boot: fix attempt to install memtest86+ on non-x86 hardware

--- common/boot.nix
+++ common/boot.nix
@@ -1,10 +1,13 @@
 { config, lib, pkgs, ... }:
 
+let
+  inherit (lib) mkIf;
+in
 {
   boot.loader = {
     systemd-boot.enable = true;
     systemd-boot.editor = false;
-    systemd-boot.memtest86.enable = true;
+    systemd-boot.memtest86.enable = mkIf config.nixpkgs.hostPlatform.isx86 true;
     efi.canTouchEfiVariables = true;
   };
 }
