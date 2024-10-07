2a2f1674a3fd1976e9ec18c42c21c964c45a1d9a
Katherina Walshe-Grey <git@qenya.tel>
2024-10-07 23:31:55 +0100

    qenya/xdg-mime-apps: file association for .zip

--- home/qenya/xdg-mime-apps.nix
+++ home/qenya/xdg-mime-apps.nix
@@ -7,13 +7,14 @@ in
   xdg.mimeApps = {
     enable = isGraphical;
     defaultApplications = {
-      "x-scheme-handler/http" = "firefox.desktop";
-      "x-scheme-handler/https" = "firefox.desktop";
-      "x-scheme-handler/mailto" = "org.gnome.Evolution.desktop"; # TODO: email on KDE - is Kontact any good?
+      "application/zip" = [ "org.gnome.FileRoller.desktop" "org.kde.ark.desktop" ];
       "image/gif" = [ "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" ];
       "image/jpeg" = [ "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" ];
       "image/png" = [ "org.gnome.Loupe.desktop" "org.kde.gwenview.desktop" ];
       "text/plain" = [ "org.gnome.TextEditor.desktop" "org.kde.kate.desktop" ];
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/mailto" = "org.gnome.Evolution.desktop"; # TODO: email on KDE - is Kontact any good?
     };
   };
 }
