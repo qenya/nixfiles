c573da7c347f8d7c8c9083fc5740fd4e502e2233
Katherina Walshe-Grey <git@qenya.tel>
2024-11-24 15:36:30 +0000

    misc: tone down aggression on nix garbage collector

--- common/misc.nix
+++ common/misc.nix
@@ -1,7 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  nix.gc.automatic = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    randomizedDelaySec = "45min";
+  };
   nix.optimise.automatic = true;
   services.fstrim.enable = true;
 }
