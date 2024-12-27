c6a16f25efad6ba27ab579387314df2e4cb78fed
Katherina Walshe-Grey <git@qenya.tel>
2024-12-27 01:38:14 +0000

    qenya/dconf: allow sound over 100%

--- home/qenya/dconf/default.nix
+++ home/qenya/dconf/default.nix
@@ -11,6 +11,7 @@ in
   dconf.settings = {
     "org/gnome/settings-daemon/plugins/color".night-light-enabled = true;
     "org/gnome/desktop/sound".event-sounds = false;
+    "org/gnome/desktop/sound".allow-volume-above-100-percent = true;
   };
 
   imports = [
