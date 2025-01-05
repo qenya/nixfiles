eb557507d07ba7402ad474d2c982bc35ce649fdd
Katherina Walshe-Grey <git@qenya.tel>
2025-01-05 14:27:55 +0000

    misc: garbage-collect nixos profiles older than 30 days

--- common/misc.nix
+++ common/misc.nix
@@ -5,6 +5,7 @@
     automatic = true;
     dates = "weekly";
     randomizedDelaySec = "45min";
+    options = "--delete-older-than 30d";
   };
   nix.optimise.automatic = true;
   services.fstrim.enable = true;
