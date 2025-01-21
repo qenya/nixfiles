0665be0db9c699f4b0e754e9ea276e146dfc4c12
Katherina Walshe-Grey <git@qenya.tel>
2025-01-21 13:59:21 +0000

    elucredassa: don't sleep when lid shut

--- hosts/elucredassa/hardware.nix
+++ hosts/elucredassa/hardware.nix
@@ -4,5 +4,8 @@
   hardware.enableAllFirmware = true;
   hardware.cpu.intel.updateMicrocode = true;
   services.fwupd.enable = true;
+
+  # this is an old laptop
+  services.logind.lidSwitch = "ignore";
 }
 
