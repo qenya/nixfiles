8f3b3adac118712e4d21dca142f761bb37950a26
Katherina Walshe-Grey <git@qenya.tel>
2025-01-21 23:19:15 +0000

    elucredassa: Get IPv6 connectivity through HE.net tunnel broker

--- hosts/elucredassa/networking.nix
+++ hosts/elucredassa/networking.nix
@@ -3,14 +3,34 @@
 {
   systemd.network.enable = true;
   networking.useDHCP = false;
-  
+
   systemd.network.networks."10-wan" = {
     matchConfig.Name = "enp1s0f1";
     networkConfig = {
       DHCP = "ipv4";
       IPv6AcceptRA = true;
+      Tunnel = "sit-he-ipv6";
     };
     linkConfig.RequiredForOnline = "routable";
+  };
+
+  systemd.network.netdevs."25-he-ipv6" = {
+    netdevConfig = {
+      Name = "sit-he-ipv6";
+      Kind = "sit";
+      Description = "Hurricane Electric IPv6 Tunnel";
+    };
+
+    tunnelConfig = {
+      Remote = "216.66.88.98";
+      TTL = 255;
+    };
+  };
+
+  systemd.network.networks."25-he-ipv6" = {
+    matchConfig.Name = "sit-he-ipv6";
+    networkConfig.Address = [ "2001:470:1f1c:3e::2/64" ];
+    routes = [{ Destination = [ "::/0" ]; }];
   };
 
   birdsong.peering = {
