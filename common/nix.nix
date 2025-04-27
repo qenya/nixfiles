9c39440238f04c1f96d3248d81c9b6b29e96da08
Katherina Walshe-Grey <git@qenya.tel>
2025-04-27 13:30:37 +0100

    nix: Remove insecure package exception for electron 31
    Feishin has updated: https://github.com/jeffvli/feishin/issues/879

--- common/nix.nix
+++ common/nix.nix
@@ -10,12 +10,4 @@
   nix.nixPath = [ "nixpkgs=flake:nixpkgs" ];
   nixpkgs.config.allowUnfree = true;
   nix.settings.trusted-users = [ "@wheel" ];
-
-  # this is a dependency of feishin (used in qenya's home-manager). it does not actually have a known vulnerability,
-  # it's just unsuspported because Electron's support cycle is a ludicrously short 6 months.
-  # feishin's dev is going to be rewriting it without Electron (as "audioling").
-  # modern software development was a mistake.
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-31.7.7"
-  ];
 }
