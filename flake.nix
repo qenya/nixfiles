7dc684cce3b3e9bb8ef666a78766d0d67e6ff239
Katherina Walshe-Grey <git@qenya.tel>
2025-05-16 17:05:42 +0100

    flake: Update lix to 2.93.0

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     lix-module = {
       # lix haven't figured out automatic updates yet: https://git.lix.systems/lix-project/nixos-module/issues/39
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
