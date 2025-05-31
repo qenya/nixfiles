86c977d8ca3372be5055b263569c2bbe43c72a31
Katherina Walshe-Grey <git@qenya.tel>
2025-06-01 00:23:36 +0100

    qenya/dconf: Disable new GNOME 48 wellbeing features

--- home/qenya/dconf/wellbeing.nix
+++ home/qenya/dconf/wellbeing.nix
@@ -1,8 +1,19 @@
 { config, lib, pkgs, ... }:
 
+# These features are cool and I would like to keep trying them, but they are
+# horribly bugged in GNOME 48.1. Consider re-enabling them when 48.2 is
+# released. See, e.g.:
+# https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8289
+# https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8299
+# https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8305
+# https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8376
+# https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/8398
+
 {
   dconf.settings = {
-    "org/gnome/desktop/screen-time-limits".daily-limit-enabled = true;
-    "org/gnome/desktop/break-reminders".selected-breaks = [ "eyesight" "movement" ];
+    # "org/gnome/desktop/screen-time-limits".daily-limit-enabled = true;
+    # "org/gnome/desktop/break-reminders".selected-breaks = [ "eyesight" "movement" ];
+    "org/gnome/desktop/screen-time-limits".daily-limit-enabled = false;
+    "org/gnome/desktop/break-reminders".selected-breaks = [ ];
   };
 }
