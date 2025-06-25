fa61c1523b6e4c4e1af29c3c1375a306671534f4
Katherina Walshe-Grey <git@qenya.tel>
2025-06-25 01:14:54 +0100

    boot: Enable resolved/DNS-over-TLS

--- common/boot.nix
+++ common/boot.nix
@@ -10,4 +10,13 @@ in
     systemd-boot.memtest86.enable = mkIf config.nixpkgs.hostPlatform.isx86 true;
     efi.canTouchEfiVariables = true;
   };
+
+  services.resolved = {
+    enable = true;
+    fallbackDns = [ ];
+    dnsovertls = "true";
+    extraConfig = ''
+      DNS=2a07:e340::4#base.dns.mullvad.net 194.242.2.4#base.dns.mullvad.net
+    '';
+  };
 }
