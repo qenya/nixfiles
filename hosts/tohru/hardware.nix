598f6245c87a5720e75f3576e245545f825afff9
Katherina Walshe-Grey <git@qenya.tel>
2026-02-24 15:07:29 +0000

    tohru: Enable Intel integrated graphics hardware acceleration

--- hosts/tohru/hardware.nix
+++ hosts/tohru/hardware.nix
@@ -5,6 +5,16 @@
   hardware.cpu.intel.updateMicrocode = true;
   services.fwupd.enable = true;
 
+  services.xserver.videoDrivers = [ "modesetting" ];
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      vpl-gpu-rt
+      intel-compute-runtime
+    ];
+  };
+
   services.printing.drivers = [ pkgs.hplip ];
 }
 
