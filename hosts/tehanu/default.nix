cc4826e4a167475b0f8a4e9b158eac1d5b6420c4
Katherina Walshe-Grey <git@qenya.tel>
2026-04-25 12:02:05 +0100

    tehanu: Kubernetes master node (WIP)

--- hosts/tehanu/default.nix
+++ hosts/tehanu/default.nix
@@ -15,5 +15,24 @@
 
   qenya.base-server.enable = true;
 
+    randomcat.services.zfs.datasets = {
+    "rpool_tehanu/state" = { mountpoint = "none"; };
+    "rpool_tehanu/state/cfssl" = { mountpoint = config.services.cfssl.dataDir; };
+    "rpool_tehanu/state/etcd" = { mountpoint = config.services.etcd.dataDir; };
+    "rpool_tehanu/state/kubernetes" = { mountpoint = config.services.kubernetes.dataDir; };
+  };
+
+  services.sanoid.datasets."rpool_tehanu/state" = {
+    useTemplate = [ "production" ];
+    recursive = "zfs";
+  };
+
+  services.kubernetes = {
+    roles = [ "master" ];
+    masterAddress = "100.77.194.23"; # tehanu tailscale ip
+    # apiserver.advertiseAddress = "100.77.194.23";
+  };
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 6443 2379 2380 10250 10259 10257 ];
+
   system.stateVersion = "23.11";
 }
